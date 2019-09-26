0fdd429a7b46244c5170bf11911157a3fc89814f
Adrian-Stefan Mares <adrian.mares@thethingsnetwork.org>
2019-09-26 19:41:16 +0200

    as: Add application packages RPC implementation

--- pkg/ttnpb/field_mask_validation.go
+++ pkg/ttnpb/field_mask_validation.go
@@ -79,6 +79,11 @@ var AllowedFieldMaskPathsForRPC = map[string][]string{
 	"/ttn.lorawan.v3.ApplicationPubSubRegistry/List": ApplicationPubSubFieldPathsNested,
 	"/ttn.lorawan.v3.ApplicationPubSubRegistry/Set":  ApplicationPubSubFieldPathsNested,
 
+	// Application Packages:
+	"/ttn.lorawan.v3.ApplicationPackageRegistry/Get":  ApplicationPackageAssociationFieldPathsNested,
+	"/ttn.lorawan.v3.ApplicationPackageRegistry/List": ApplicationPackageAssociationFieldPathsNested,
+	"/ttn.lorawan.v3.ApplicationPackageRegistry/Set":  ApplicationPackageAssociationFieldPathsNested,
+
 	// Application Links:
 	"/ttn.lorawan.v3.As/GetLink": ApplicationLinkFieldPathsNested,
 	"/ttn.lorawan.v3.As/SetLink": ApplicationLinkFieldPathsNested,
