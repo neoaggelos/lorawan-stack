e25cfc1674ae77c05f4a29af3084856719362cbc
Aggelos Kolaitis <aggelos.kolaitis@thethingsindustries.com>
2020-11-26 03:24:37 +0200

    dr: Test all end device model fields

--- pkg/devicerepository/store/remote_test.go
+++ pkg/devicerepository/store/remote_test.go
@@ -56,8 +56,8 @@ firmwareVersions:
   hardwareVersions:
   - 1.0
   profiles:
-    EU863-870: {codec: foo-codec, profile-id: profile1, lorawanCertified: true}
-    US902-928: {codec: foo-codec, profile-id: profile2, lorawanCertified: true}`),
+    EU863-870: {codec: foo-codec, id: profile1, lorawanCertified: true}
+    US902-928: {codec: foo-codec, id: profile2, lorawanCertified: true}`),
 		"vendor/foo-vendor/dev2.yaml": []byte(`
 name: Device 2
 description: My Description 2
@@ -67,10 +67,9 @@ hardwareVersions:
   partNumber: P4RTN02
 firmwareVersions:
 - version: 1.1
-  hardwareVersions:
-  - 2.0
+  hardwareVersions: [2.0]
   profiles:
-    EU433: {codec: foo-codec, profile-id: profile2, lorawanCertified: true}
+   EU433: {codec: foo-codec, id: profile2, lorawanCertified: true}
 sensors:
 - temperature`),
 		"vendor/foo-vendor/profile1.yaml": []byte(`
@@ -89,6 +88,57 @@ downlinkEncoder: {fileName: c.js}`),
 		"vendor/foo-vendor/a.js": []byte("uplink decoder"),
 		"vendor/foo-vendor/b.js": []byte("downlink decoder"),
 		"vendor/foo-vendor/c.js": []byte("downlink encoder"),
+
+		"vendor/full-vendor/index.yaml": []byte(`endDevices: [full-device]`),
+		// full-vendor/full-device sets and tests all fields
+		"vendor/full-vendor/full-device.yaml": []byte(`
+name: Full Device
+description: A description
+hardwareVersions:
+- version: 0.1
+  numeric: 1
+  partNumber: 0A0B
+- version: 0.2
+  numeric: 2
+  partNumber: 0A0C
+firmwareVersions:
+  - version: 1.0
+    hardwareVersions: [0.1, 0.2]
+    profiles:
+      EU863-870: {id: full-profile2}
+      US902-928: {id: full-profile, codec: codec}
+sensors: [temperature, gas]
+dimensions:
+  width: 1
+  height: 2
+  diameter: 3
+  length: 4
+weight: 5
+battery:
+  replaceable: true
+  type: AAA
+operatingConditions:
+  temperature: {min: 1, max: 2}
+  relativeHumidity: {min: 3, max: 4}
+ipCode: IP67
+keyProvisioning: [custom]
+keySecurity: security
+photos:
+  main: a.jpg
+  other: [b.jpg, c.jpg]
+videos:
+  main: a.mp4
+  other: [b.mp4, "https://youtube.com/watch?v=c.mp4"]
+productURL: https://product.vendor.io
+datasheetURL: https://production.vendor.io/datasheet.pdf
+compliances:
+  safety:
+  - {body: IEC, norm: EN, standard: 62368-1}
+  - {body: IEC, norm: EN, standard: 60950-22}
+  radioEquipment:
+  - {body: ETSI, norm: EN, standard: 301 489-1, version: 2.2.0}
+  - {body: ETSI, norm: EN, standard: 301 489-3, version: 2.1.0}
+additionalRadios: [nfc, wifi]`),
 	}
 )
 
@@ -264,7 +314,7 @@ func TestRemoteStore(t *testing.T) {
 						{
 							Version:    "2.0",
 							Numeric:    2,
-							PartNumber: "P4RTN0",
+							PartNumber: "P4RTN02",
 						},
 					},
 					FirmwareVersions: []*ttnpb.EndDeviceModel_FirmwareVersion{
@@ -282,6 +332,122 @@ func TestRemoteStore(t *testing.T) {
 					},
 					Sensors: []string{"temperature"},
 				},
+			})
+		})
+
+		t.Run("Full", func(t *testing.T) {
+			a := assertions.New(t)
+			list, err := s.ListModels(store.ListModelsRequest{
+				BrandID: "full-vendor",
+				Paths:   ttnpb.EndDeviceModelFieldPathsNested,
+			})
+			a.So(err, should.BeNil)
+			a.So(list[0], should.Resemble, &ttnpb.EndDeviceModel{
+				ModelID:     "full-device",
+				Name:        "Full Device",
+				Description: "A description",
+				HardwareVersions: []*ttnpb.EndDeviceModel_Version{
+					{
+						Version:    "0.1",
+						Numeric:    1,
+						PartNumber: "0A0B",
+					},
+					{
+						Version:    "0.2",
+						Numeric:    2,
+						PartNumber: "0A0C",
+					},
+				},
+				FirmwareVersions: []*ttnpb.EndDeviceModel_FirmwareVersion{
+					{
+						Version:          "1.0",
+						HardwareVersions: []string{"0.1", "0.2"},
+						Profiles: map[string]*ttnpb.EndDeviceModel_FirmwareVersion_Profile{
+							"EU_863_870": {
+								CodecID:   "",
+								ProfileID: "full-profile2",
+							},
+							"US_902_928": {
+								CodecID:   "codec",
+								ProfileID: "full-profile",
+							},
+						},
+					},
+				},
+				Sensors: []string{"temperature", "gas"},
+				Dimensions: &ttnpb.EndDeviceModel_Dimensions{
+					Width:    1,
+					Height:   2,
+					Diameter: 3,
+					Length:   4,
+				},
+				Weight: 5,
+				Battery: &ttnpb.EndDeviceModel_Battery{
+					Replaceable: &pbtypes.BoolValue{
+						Value: true,
+					},
+					Type: "AAA",
+				},
+				OperatingConditions: &ttnpb.EndDeviceModel_OperatingConditions{
+					Temperature: &ttnpb.EndDeviceModel_OperatingConditions_Limits{
+						Min: &pbtypes.FloatValue{
+							Value: 1,
+						},
+						Max: &pbtypes.FloatValue{
+							Value: 2,
+						},
+					},
+					RelativeHumidity: &ttnpb.EndDeviceModel_OperatingConditions_Limits{
+						Min: &pbtypes.FloatValue{
+							Value: 3,
+						},
+						Max: &pbtypes.FloatValue{
+							Value: 4,
+						},
+					},
+				},
+				IPCode:          "IP67",
+				KeyProvisioning: []string{"custom"},
+				KeySecurity:     "security",
+				Photos: &ttnpb.EndDeviceModel_Photos{
+					Main:  "a.jpg",
+					Other: []string{"b.jpg", "c.jpg"},
+				},
+				Videos: &ttnpb.EndDeviceModel_Videos{
+					Main:  "a.mp4",
+					Other: []string{"b.mp4", "https://youtube.com/watch?v=c.mp4"},
+				},
+				ProductURL:   "https://product.vendor.io",
+				DatasheetURL: "https://production.vendor.io/datasheet.pdf",
+				Compliances: &ttnpb.EndDeviceModel_Compliances{
+					Safety: []*ttnpb.EndDeviceModel_Compliances_Compliance{
+						{
+							Body:     "IEC",
+							Norm:     "EN",
+							Standard: "62368-1",
+						},
+						{
+							Body:     "IEC",
+							Norm:     "EN",
+							Standard: "60950-22",
+						},
+					},
+					RadioEquipment: []*ttnpb.EndDeviceModel_Compliances_Compliance{
+						{
+							Body:     "ETSI",
+							Norm:     "EN",
+							Standard: "301 489-1",
+							Version:  "2.2.0",
+						},
+						{
+							Body:     "ETSI",
+							Norm:     "EN",
+							Standard: "301 489-3",
+							Version:  "2.1.0",
+						},
+					},
+				},
+				AdditionalRadios: []string{"nfc", "wifi"},
 			})
 		})
 	})
